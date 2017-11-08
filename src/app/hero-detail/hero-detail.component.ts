e45cf0bbd8b51dc160e15a95c5c2d3f4dfad0cd6
Zongkun <nkzongkun@163.com>
2017-11-08 18:16:11 +0800

    Revert "hero-detail增加FromGroup"
    This reverts commit ca8f1d14a4fcde86e359e4b3587f0d43d912b568.

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,65 +1,19 @@
-import { Component, Input, OnChanges, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { Component, Input, OnInit } from '@angular/core';
 
 import { Hero } from '../model/data-model';
-import { HeroService } from '../service/hero.service';
 
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
   styleUrls: ['./hero-detail.component.css']
 })
-export class HeroDetailComponent implements OnInit, OnChanges {
+export class HeroDetailComponent implements OnInit {
   @Input() hero: Hero;
-  heroForm: FormGroup;
-  nameChangeLog: string[] = [];
 
-  constructor(private fb: FormBuilder,
-              private heroService: HeroService) {
-    this.createForm();
-    this.logNameChange();
+  constructor() {
   }
 
   ngOnInit() {
-  }
-
-  createForm() {
-    this.heroForm = this.fb.group({
-      name: ''
-    });
-  }
-
-  ngOnChanges() {
-    this.heroForm.reset({
-      name: this.hero.name
-    });
-  }
-
-  onSubmit() {
-    this.hero = this.prepareSaveHero();
-    this.heroService.updateHero(this.hero).subscribe();
-    this.ngOnChanges();
-  }
-
-  prepareSaveHero(): Hero {
-    const formModel = this.heroForm.value;
-    const saveHero: Hero = {
-      id: this.hero.id,
-      name: formModel.name as string,
-      addresses: this.hero.addresses
-    };
-    return saveHero;
-  }
-
-  revert() {
-    this.ngOnChanges();
-  }
-
-  logNameChange() {
-    const nameControl = this.heroForm.get('name');
-    nameControl.valueChanges.forEach(
-      (value: string) => this.nameChangeLog.push(value)
-    );
   }
 
 }
