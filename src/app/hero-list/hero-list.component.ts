16f4afeee6592ee0cc9890ab8bbb0448530aa0d5
Zongkun <nkzongkun@163.com>
2017-12-20 23:31:10 +0800

    练习rxjs

--- src/app/hero-list/hero-list.component.ts
+++ src/app/hero-list/hero-list.component.ts
@@ -4,7 +4,12 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/finally';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/throttleTime';
+import 'rxjs/add/operator/scan';
 import 'rxjs/add/observable/of';
+import 'rxjs/add/observable/from';
+// import 'rxjs/add/observable/interval';
+import 'rxjs/add/observable/fromEvent';
 import { Hero } from '../model/data-model';
 import { HeroService } from '../service/hero.service';
 
@@ -25,11 +30,19 @@ export class HeroListComponent implements OnInit {
   ngOnInit() {
     Observable.of(1, 2, 3).map(x => x + '!!!')
       .subscribe(x => console.log(x));
+    Observable.from(['hello', 'buddy'])
+      .subscribe(x => console.log(x));
+    // Observable.interval(1000)
+    //   .subscribe(x => console.log(x));
+
     let observable = Observable.create(function subsribe(observer) {
-      observer.next(11);
-      observer.next(22);
+      observer.next(1);
+      observer.next(2);
       // observer.error('something went really wrong...');
-      observer.complete();
+      setTimeout(() => {
+        observer.next(3);
+        observer.complete();
+      }, 1000);
     });
     let myObserver = {
       next: x => console.log('Observer got a next value: ' + x),
@@ -43,6 +56,12 @@ export class HeroListComponent implements OnInit {
       err => console.error('Observer got an error: ' + err),
       () => console.log('Observer got a complete notification')
     );
+
+    let button = document.querySelector('#button');
+    Observable.fromEvent(button, 'click')
+      .throttleTime(1000)
+      .scan((count) => +count + 1, 0)
+      .subscribe(count => console.log(`Click ${count} times`));
 
     this.getHeroes();
     this.results = this.http.get('assets/api/items.json');
